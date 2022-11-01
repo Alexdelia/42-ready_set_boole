8501dcc3b3f1bb4147ddb58d71283f1c59813d6b
Alexdelia <alexandre.delille.57@gmail.com>
2022-11-01 14:40:36 +0100

    optimized

--- src/ex01/multiplier.rs
+++ src/ex01/multiplier.rs
@@ -2,8 +2,10 @@ use crate::ex00::adder::adder;
 
 pub fn multiplier(a: u32, b: u32) -> u32 {
     let mut x = 0;
-    for _ in 0..b {
-        x = adder(x, a);
+    for i in 0..32 {
+        if (b & (1 << i)) != 0 {
+            x = adder(x, a << i);
+        }
     }
     x
 }
